c1304b684dcc9075a4a26bfe26e3b884c0585fc8
Desenvolvedor <desenvolvedor@localhost.localdomain>
2018-09-02 20:44:06 -0300

    -

--- centosJava8SeleniumSBWS/iniciarColeta.sh
+++ centosJava8SeleniumSBWS/iniciarColeta.sh
@@ -1,3 +1,11 @@
+su -c "setenforce 0"
 sudo docker build -t coleta_mufato .
-sudo docker run -ti -v coleta:/home/superBits/projetos/vip/coletas/mufato/ coleta_mufato
+mkdir /home/coleta 
+mkdir /home/coleta/config 
+#chmod +777 /home/coleta
+cp ./categoria.txt /home/coleta/
+sudo docker run -ti -v /home/coleta:/home/superBits/projetos/vip/coletas/mufato/ -v  /home/coleta/config:/home/superBits/projetos/Casa_Nova/source/ColetorDeDados/arquivosDeEntidade/arquivos/modulos/Mufato/  coleta_mufato
+
+
+# /home/coleta/config:/home/superBits/projetos/Casa_Nova/source/ColetorDeDados/arquivosDeEntidade/arquivos/modulos/Mufato/
 
