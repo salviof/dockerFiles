8988222d469953c83ce9157227a1fb32c22b23c6
Desenvolvedor <desenvolvedor@localhost.localdomain>
2018-09-02 01:00:39 -0300

    -

--- centosJava8SeleniumSBWS/installChromeDriver.sh
+++ centosJava8SeleniumSBWS/installChromeDriver.sh
@@ -12,26 +12,15 @@ SELENIUM_STANDALONE_VERSION=3.8.1
 SELENIUM_SUBDIR=$(echo "$SELENIUM_STANDALONE_VERSION" | cut -d"." -f-2)
 
 # Remove existing downloads and binaries so we can start from scratch.
-sudo yum remove google-chrome-stable
+yum remove google-chrome-stable
 rm ~/selenium-server-standalone-*.jar
 rm ~/chromedriver_linux64.zip
-sudo rm /usr/local/bin/chromedriver
-sudo rm /usr/local/bin/selenium-server-standalone.jar
+rm /usr/local/bin/chromedriver
+rm /usr/local/bin/selenium-server-standalone.jar
 
 # Install dependencies.
 #sudo yum update
-sudo yum install -y unzip  xvfb libxi6 libgconf-2-4
-
-# Install Chrome.
-#vi /etc/yum.repos.d/google-chrome.repo
-
-# Make sure you have below info in the file(remove hash).
-#[google-chrome]
-#name=google-chrome
-#baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
-#enabled=1
-#gpgcheck=1
-#gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
+yum install -y unzip  xvfb libxi6 libgconf-2-4
 
 yum install -y google-chrome-stable
 
@@ -39,6 +28,6 @@ yum install -y google-chrome-stable
 wget -N https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
-sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
-sudo chown root:root /usr/local/bin/chromedriver
-sudo chmod 0755 /usr/local/bin/chromedriver
+mv -f ~/chromedriver /usr/local/bin/chromedriver
+chown root:root /usr/local/bin/chromedriver
+chmod 0755 /usr/local/bin/chromedriver
