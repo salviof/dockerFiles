ecb827ae7b8905ab45e09a7b4854a4ab4e3d6c3e
Desenvolvedor <desenvolvedor@localhost.localdomain>
2018-09-02 12:15:03 -0300

    --

---
+++ centosJava8SeleniumSBWS/iniciarColeta.sh
@@ -0,0 +1,3 @@
+sudo docker build -t coleta_mufato .
+sudo docker run -ti -v coleta:/home/superBits/projetos/vip/coletas/mufato/ coleta_mufato
+
--- centosJava8SeleniumSBWS/installChromeDriver.sh
+++ centosJava8SeleniumSBWS/installChromeDriver.sh
@@ -13,10 +13,10 @@ SELENIUM_SUBDIR=$(echo "$SELENIUM_STANDALONE_VERSION" | cut -d"." -f-2)
 
 # Remove existing downloads and binaries so we can start from scratch.
 yum remove google-chrome-stable
-rm ~/selenium-server-standalone-*.jar
-rm ~/chromedriver_linux64.zip
-rm /usr/local/bin/chromedriver
-rm /usr/local/bin/selenium-server-standalone.jar
+#rm ~/selenium-server-standalone-*.jar
+#rm ~/chromedriver_linux64.zip
+#rm /usr/local/bin/chromedriver
+#rm /usr/local/bin/selenium-server-standalone.jar
 
 # Install dependencies.
 #sudo yum update
@@ -25,9 +25,9 @@ yum install -y unzip  xvfb libxi6 libgconf-2-4
 yum install -y google-chrome-stable
 
 # Install ChromeDriver.
-wget -N https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip -P ~/
-unzip ~/chromedriver_linux64.zip -d ~/
-rm ~/chromedriver_linux64.zip
-mv -f ~/chromedriver /usr/local/bin/chromedriver
+#wget -N https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip -P ~/
+#unzip ~/chromedriver_linux64.zip -d ~/
+#rm ~/chromedriver_linux64.zip
+#mv -f ~/chromedriver /usr/local/bin/chromedriver
 chown root:root /usr/local/bin/chromedriver
 chmod 0755 /usr/local/bin/chromedriver
