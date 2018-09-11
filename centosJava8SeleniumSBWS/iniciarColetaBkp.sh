a52d1211e8ed6c4830380e4a44e631a71cfac854
Salvio Furbino Elias <salviof@gmail.com>
2018-09-11 20:30:13 -0300

    --

--- centosJava8SeleniumSBWS/iniciarColetaBkp.sh
+++ centosJava8SeleniumSBWS/iniciarColetaBkp.sh
@@ -1,11 +1,11 @@
-docker kill $(docker ps -q)
+#docker kill $(docker ps -q)
 su -c "setenforce 0"
 sudo docker build -t coleta_mufato .
 mkdir /home/coleta 
 mkdir /home/coleta/config 
 #chmod +777 /home/coleta
 cp ./categoria.txt /home/coleta/
-sudo docker run -d  --restart always -v /home/coleta:/home/superBits/projetos/vip/coletas/mufato/ -v  /home/coleta/config:/home/superBits/projetos/Casa_Nova/source/ColetorDeDados/arquivosDeEntidade/arquivos/modulos/Mufato/  coleta_mufato 
+sudo docker run -d  --restart always -v /home/coleta/:/home/superBits/projetos/vip/coletas/mufato/ -v  /home/coleta/config/:/home/superBits/projetos/Casa_Nova/source/ColetorDeDados/arquivosDeEntidade/arquivos/modulos/Mufato/  coleta_mufato 
 
 # /home/coleta/config:/home/superBits/projetos/Casa_Nova/source/ColetorDeDados/arquivosDeEntidade/arquivos/modulos/Mufato/
 
