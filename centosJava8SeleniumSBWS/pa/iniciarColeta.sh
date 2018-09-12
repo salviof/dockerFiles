2b3e3bd6750d32e2bc57b52a73e9eb16c198f52c
Salvio Furbino Elias <salviof@gmail.com>
2018-09-11 23:33:38 -0300

    --

--- centosJava8SeleniumSBWS/pa/iniciarColeta.sh
+++ centosJava8SeleniumSBWS/pa/iniciarColeta.sh
@@ -1,11 +1,11 @@
-docker kill $(docker ps -q)
+#docker kill $(docker ps -q)
 su -c "setenforce 0"
-sudo docker build -t coleta_pa .
+sudo docker build -t coleta_paa .
 mkdir /home/coleta/pa 
 mkdir /home/coleta/pa/config 
 #chmod +777 /home/coleta
 cp ./ean_sem_fotos.txt /home/coleta/pa/
-sudo docker run -d  --restart always -v /home/coleta/pa/:/home/superBits/projetos/vip/coletas/paoDeAcucar/ -v  /home/coleta/pa/config/:/home/superBits/projetos/Casa_Nova/source/ColetorDeDados/arquivosDeEntidade/arquivos/modulos/PaoDeAcucar/  coleta_pa 
+sudo docker run -d  --restart always -v /home/coleta/pa/:/home/superBits/projetos/vip/coletas/paoDeAcucar/ -v  /home/coleta/pa/config/:/home/superBits/projetos/Casa_Nova/source/ColetorDeDados/arquivosDeEntidade/arquivos/modulos/PaoDeAcucar/  coleta_paa 
 
 # /home/coleta/config:/home/superBits/projetos/Casa_Nova/source/ColetorDeDados/arquivosDeEntidade/arquivos/modulos/Mufato/
 
